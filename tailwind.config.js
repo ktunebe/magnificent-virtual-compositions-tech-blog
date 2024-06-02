e414cadef65f363a5054957d1ffe7a52045b98d2
ktunebe <ktunebe@gmail.com>
2024-06-01 23:56:49 -0500

    further styled pages, added custom font (will give credit in README)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,handlebars}"],
+  purge: {
+    enabled: true,
+    content: ["./**/*.{html,js,handlebars}"],
+  },
   theme: {
     extend: {
       colors: {
         primary: '#334155',
         secondary: '#7dd3fc',
         accent: '#cbd5e1'
+      },
+      fontFamily: {
+        blox: ['blox2', 'sans-serif']
       }
     },
   },
